3313e80861434668ce5bf731d48c63484b6e376c
Alex Junio <alejunioti@gmail.com>
2022-07-29 10:14:00 -0300

    Update

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+apt update && apt install -y git
+
 # INSTALACAO DO DOCKER
 curl -fsSL https://get.docker.com | sh
-systemctl status docker
 systemctl enable docker
 
 # INSTALACAO COMPOSE
@@ -11,3 +12,5 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
+clear
+docker compose version
